c48a4709e11610cc41c0e973be3357ec8aaaee26
Zinogore <jingkang95@hotmail.com>
2026-05-14 02:54:31 +0800

    Update: removed utilities folder, added script for populating gym names

---
+++ climb-where-bot/scripts/populate_gym_names.py
@@ -0,0 +1,4 @@
+from src.app.core.database import populate_gym_names
+
+if __name__ == "__main__":
+    populate_gym_names()
--- climb-where-bot/utilities/data_handler.py
+++
@@ -1,8 +0,0 @@
-import json
-
-# read database.json
-with open("./database.json", "r") as file:
-    data = json.load(file)
-
-def get_list_of_gym_names(data=data):
-    return [ele["name"] for ele in data["data"]["climbing_gyms"]]
